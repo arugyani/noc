b37f5223cc2b6552c6951dc4a58796166824bd00
Aru Gyani <gyaniaru@gmail.com>
2025-10-18 01:24:09 -0400

    feat: right walker

--- src/book/random/walkers/biased.ts
+++ src/book/random/walkers/biased.ts
@@ -1,13 +1,27 @@
 import { random } from "@/lib/math";
 import { Point } from "@/lib/physics";
 
+export type StepFn = () => number;
+
+type Options = {
+    xStep?: StepFn;
+    yStep?: StepFn;
+}
+
 export class BiasedWalker extends Point {
+    private opts;
+
+    constructor(x: number, y: number, opts: Options = {}) {
+        super(x, y);
+        this.opts = opts;
+    } 
+
     update() {
-        let xstep = random(-2.75, 3);
-        let ystep = random(-2.75, 3);
+        let xstep = this.opts.xStep ?? (() => random(-2.75, 3));
+        let ystep = this.opts.yStep ?? (() => random(-2.75, 3));
         
-        this.x += xstep;
-        this.y += ystep;
+        this.x += xstep();
+        this.y += ystep();
     }
 
     render(ctx: CanvasRenderingContext2D) {
